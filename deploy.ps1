85f649b8a1ba98948891c57d382de37fec77ec0c
David Beck <techiscool@gmail.com>
2017-08-30 22:25:50 -0700

    [Remove] Guard for now

--- deploy.ps1
+++ deploy.ps1
@@ -1,16 +1,11 @@
-if ($(git rev-parse --abbrev-ref HEAD) -eq 'master'){
-  $published_version = $(choco list newrelic-dotnet --limitoutput).Split('|')
-  $local_nupkg = $env:NUPKG
-  $published_nupkg = "$($published_version[0]).$($published_version[1]).nupkg"
+$published_version = $(choco list newrelic-dotnet --limitoutput).Split('|')
+$local_nupkg = $env:NUPKG
+$published_nupkg = "$($published_version[0]).$($published_version[1]).nupkg"
 
-  if( $env:NUPKG -ne $published_nupkg ){
-    choco apiKey -k $env:TOKEN -source https://chocolatey.org/
-    choco push $env:NUPKG
-  }
-  else {
-      Write-Output "Published Version ($published_nupkg) already equals Build Version ($local_nupkg)"
-  }
+if( $env:NUPKG -ne $published_nupkg ){
+  choco apiKey -k $env:TOKEN -source https://chocolatey.org/
+  choco push $env:NUPKG
 }
 else {
-  Write-Output "Only the master branch can deploy"
+    Write-Output "Published Version ($published_nupkg) already equals Build Version ($local_nupkg)"
 }
